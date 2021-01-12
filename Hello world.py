572b6c3f44e96e51113cae796433f8ff95d6bfa0
UrbanTea <jacob@murfman.net>
2021-01-11 19:01:03 -0700

    Create Hello world.py

---
+++ Hello world.py
@@ -0,0 +1,9 @@
+#J Muprhy
+#January 11 2021
+#Program prints "hello world" surounded by Ascii
+import time
+print("<===========>")
+time.sleep(.2)
+print("<hello world>")
+time.sleep(.2)
+print("<===========>")
