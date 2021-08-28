568f984cf530603f210198d8302aeafc5b237aee
UrbanTea <jacob@murfman.net>
2021-08-28 03:09:32 -0600

    added a hello world program in C#

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static string output = "";
+        static void fizzbuzz(int x,int y, string word)
+        {
+            if (x % y == 0)
+            {
+                output = output + word;
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for(int i = 0; i < 100; i++)
+            {
+                fizzbuzz(i, 3, "fizz");
+                fizzbuzz(i, 5, "buzz");
+                if (output == "")
+                {
+                    output = i.ToString();
+                }
+                Console.WriteLine(output);
+                output = "";
+            }
+        }
+    }
+}
